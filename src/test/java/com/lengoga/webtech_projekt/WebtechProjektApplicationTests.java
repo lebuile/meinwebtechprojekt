418fc7f2c866ab92ba95dd9f3da07b6cd199ce2d
lebuile <le.bui@student.htw-berlin.de>
2025-04-07 13:04:35 +0200

    Erster Commit

---
+++ src/test/java/com/lengoga/webtech_projekt/WebtechProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lengoga.webtech_projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
