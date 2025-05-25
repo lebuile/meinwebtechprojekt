a363a7750e7b18e4d6aa7de0c0a43d912cbe88d6
“NgogaSandro” <Sandro.Ngoga@Student.HTW-Berlin.de>
2025-05-25 23:00:09 +0200

    anpassung für frontend-anfragen

--- src/main/java/com/lengoga/webtech_projekt/WatchlistController.java
+++ src/main/java/com/lengoga/webtech_projekt/WatchlistController.java
@@ -1,11 +1,13 @@
 package com.lengoga.webtech_projekt;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class WatchlistController {
 
     @GetMapping("/watchlist")
